ca42df983181a46ecd21d5d0795462e9728348f1
Jamesliangk9 <104614862+Jamesliangk9@users.noreply.github.com>
2024-10-14 17:06:58 +1100

    Publishing

--- obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4a76fc86cad412e6fc7ba85959218de9e56906")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
